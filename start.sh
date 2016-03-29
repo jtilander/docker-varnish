7087c445a6f9b57c5894325c759e9a7c3ea63418
Jim Tilander <jim@tilander.org>
2016-03-29 12:54:16 -0400

    Precreate cache file to prevent fragmentation

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 set -e
+CACHE_FILE=/tmp/varnishcache.bin
+
+if [ ! -f $CACHE_FILE ]; then
+	echo Creating new cache file at $CACHE_FILE with the size $CACHE_SIZE to prevent fragmentation.
+	dd if=/dev/zero of=$CACHE_FILE bs=1 count=0 seek=$CACHE_SIZE
+fi
 
 exec bash -c \
   "exec varnishd -F -u varnish \
   -f $VCL_CONFIG \
-  -s file,/tmp/varnishcache.bin,$CACHE_SIZE \
+  -s file,$CACHE_FILE,$CACHE_SIZE \
   $VARNISHD_PARAMS"
